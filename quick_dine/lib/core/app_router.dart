630d2fd73f3ab538abddc0adda5b5d22da99105a
rainmaycry <herazoacosta4@gmail.com>
2025-08-20 18:23:05 -0500

    Routes fixes :D

--- quick_dine/lib/core/app_router.dart
+++ quick_dine/lib/core/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:quick_dine/features/owner/presentation/menu_management_screen.dart';
+import 'package:quick_dine/features/owner/presentation/table_schedule_management_screen.dart';
 import '../features/common/presentation/splash_screen.dart';
 import '../features/common/presentation/welcome_screen.dart';
 import '../features/common/presentation/notifications_screen.dart';
@@ -9,6 +11,7 @@ import '../features/auth/presentation/login_screen.dart';
 import '../features/auth/presentation/signup_screen.dart';
 import '../features/auth/presentation/forgot_password_screen.dart';
 import '../features/owner/presentation/owner_dashboard_screen.dart';
+import '../features/admin/presentation/admin_dashboard_screen.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
