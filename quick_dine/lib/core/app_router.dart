e8c5193d13644d1d5542086ea92586088377bc57
rainmaycry <herazoacosta4@gmail.com>
2025-08-20 15:18:59 -0500

    more changes?

--- quick_dine/lib/core/app_router.dart
+++ quick_dine/lib/core/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
-import '../../features/common/presentation/splash_screen.dart';
-import '../../features/common/presentation/welcome_screen.dart';
-import '../../features/auth/presentation/login_screen.dart';
-import '../../features/auth/presentation/signup_screen.dart';
+import '../features/common/presentation/splash_screen.dart';
+import '../features/common/presentation/welcome_screen.dart';
+import '../features/common/presentation/notifications_screen.dart';
+import '../features/auth/presentation/login_screen.dart';
+import '../features/auth/presentation/signup_screen.dart';
+import '../features/auth/presentation/forgot_password_screen.dart';
+import '../features/owner/presentation/owner_dashboard_screen.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
@@ -19,8 +21,24 @@ class AppRouter {
         builder: (context, state) => const WelcomeScreen(),
       ),
       GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/forgot-password',
+        builder: (context, state) => const ForgotPasswordScreen(),
+      ),
+      GoRoute(
         path: '/signup',
         builder: (context, state) => const SignupScreen(),
+      ),
+      GoRoute(
+        path: '/owner-dashboard',
+        builder: (context, state) => const OwnerDashboardScreen(),
+      ),
+      GoRoute(
+        path: '/notifications',
+        builder: (context, state) => const NotificationsScreen(),
       ),
     ],
   );
